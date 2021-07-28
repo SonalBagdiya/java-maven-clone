313f0a8bfafe470ab40b794e51bbcc6b8a4f1a41
Subir Chandra Ghosh03 <Subir_Ghosh03@infosys.com>
2021-07-28 12:09:13 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,10 +4,7 @@ libraries{
     credentialsId = "subir_infygit_cred"
   }
   maven
-  sonarqube {
-    sonarTokenCred = "sonar_token"
-	buildTool = "maven"
-  }
+  sonarqube
   jfrog {
     credentialId = "artifactory_API_key"
     releaseRepo = "devopscoe-maven-virtual"
